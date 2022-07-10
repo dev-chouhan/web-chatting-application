9c2cb39435feeae08bba8dc83b5ccd90c7c8a77d
Dev_Chouhan <devchouhan24git@gmail.com>
2022-07-10 20:23:46 +0530

    rendering time from UST to IST

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ function formatAMPM(date) {
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
+    var strTime = (hours + 5) + ':' + (minutes + 30) + ' ' + ampm;
     return strTime;
 }
 function dateInHome(today){
